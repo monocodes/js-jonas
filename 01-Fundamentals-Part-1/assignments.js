4485a99cd8c332786f4813559e59a7e68ef518a6
monocodes <code@wandering-mono.top>
2023-07-21 00:52:09 +0400

    10-Practice-Assigments

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,7 @@
+let country = "Armenia";
+let continent = "Eurasia";
+let population = 2.791;
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,26 @@
 let js = "amazing";
-if (js == "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+let _function = 28;
+
+let person = "jonas"; // first uppercase only for OOP
+let PI = 3.1415; // uppercase for constants
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
