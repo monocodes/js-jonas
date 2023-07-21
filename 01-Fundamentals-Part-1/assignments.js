b8fbd753125fa49615e249323640f5b3c63b5d10
monocodes <code@wandering-mono.top>
2023-07-21 20:48:31 +0400

    13-Basic-Operators

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -2,12 +2,13 @@
 /* 
 let country = "Armenia";
 let continent = "Eurasia";
-let population = 2.791;
+let population = 2;
 
 console.log(country);
 console.log(continent);
 console.log(population);
  */
+
 // LECTURE: Data Types
 /* 
 let isIsland = false;
@@ -17,9 +18,42 @@ console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
  */
+
 // LECTURE: let, const and var
+/* 
 language = "Armenian";
 const country = "Armenia";
 const continent = "Eurasia";
 const isIsland = false;
 // isIsland = true
+ */
+
+// LECTURE: Basic Operators
+/* 
+let population = 2;
+const language = "Armenian";
+const country = "Armenia";
+const continent = "Eurasia";
+
+console.log(population / 2);
+
+population++;
+console.log(population);
+
+let finlandPopulation = 6;
+console.log(population > finlandPopulation);
+
+let averagePopulation = 33;
+console.log(population < averagePopulation);
+
+const description =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+
+console.log(description);
+ */
