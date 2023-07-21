1227c82988db959957d8c7520cd9377967bbce2d
monocodes <code@wandering-mono.top>
2023-07-21 18:48:02 +0400

    13-let-const-var

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,3 +1,5 @@
+// LECTURE: Values and Variables
+/* 
 let country = "Armenia";
 let continent = "Eurasia";
 let population = 2.791;
@@ -5,3 +7,19 @@ let population = 2.791;
 console.log(country);
 console.log(continent);
 console.log(population);
+ */
+// LECTURE: Data Types
+/* 
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+ */
+// LECTURE: let, const and var
+language = "Armenian";
+const country = "Armenia";
+const continent = "Eurasia";
+const isIsland = false;
+// isIsland = true
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -10,6 +11,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable name conventions
 let jonas_matilda = "JM";
 let $function = 27;
 let _function = 28;
@@ -24,3 +26,40 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
+*/
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+// By this way, JS creates a property on the global object, not just variable
+lastName = "Schmedtmann";
+console.log(lastName);
