3d5a6e79d6e5a22998d1629f149161c9c86f75b5
monocodes <code@wandering-mono.top>
2023-07-23 18:06:36 +0400

    16-Challenge-1-Video-Solution

---
+++ coding-challenges/01-coding-challenge/01-coding-challenge-my.js
@@ -0,0 +1,75 @@
+// BMI V1 Data 1
+
+// Mark
+let mass = 78;
+let height = 1.69;
+let bmi = mass / height ** 2;
+markBMI = bmi;
+
+// John
+mass = 92;
+height = 1.95;
+bmi = mass / height ** 2;
+johnBMI = bmi;
+console.log(markBMI, johnBMI);
+
+// BMI Comparison
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+// BMI V1 Data 2
+
+// Mark
+mass = 95;
+height = 1.88;
+bmi = mass / height ** 2;
+markBMI = bmi;
+
+// John
+mass = 85;
+height = 1.76;
+bmi = mass / height ** 2;
+johnBMI = bmi;
+console.log(markBMI, johnBMI);
+
+// BMI Comparison
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+// BMI V2 Data 1
+
+// Mark
+mass = 78;
+height = 1.69;
+bmi = mass / (height * height);
+markBMI = bmi;
+
+// John
+mass = 92;
+height = 1.95;
+bmi = mass / (height * height);
+johnBMI = bmi;
+console.log(markBMI, johnBMI);
+
+// BMI Comparison
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+// BMI V2 Data 2
+
+// Mark
+mass = 95;
+height = 1.88;
+bmi = mass / (height * height);
+markBMI = bmi;
+
+// John
+mass = 85;
+height = 1.76;
+bmi = mass / (height * height);
+johnBMI = bmi;
+console.log(markBMI, johnBMI);
+
+// BMI Comparison
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
