2693746543fccc94802a42e680de8ce14dc9c27f
monocodes <code@wandering-mono.top>
2023-07-28 21:56:50 +0400

    21-Truthy-and-Falsy-Values

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,4 +1,4 @@
-/* LECTURE: Values and Variables */
+// LECTURE: Values and Variables
 /* 
 let country = "Armenia";
 let continent = "Eurasia";
@@ -19,7 +19,7 @@ console.log(typeof country);
 console.log(typeof language);
  */
 
-/* LECTURE: let, const and var */
+// LECTURE: let, const and var
 /* 
 language = "Armenian";
 const country = "Armenia";
@@ -28,7 +28,7 @@ const isIsland = false;
 // isIsland = true
  */
 
-/* LECTURE: Basic Operators */
+// LECTURE: Basic Operators
 /* 
 let population = 2;
 const language = "Armenian";
@@ -58,7 +58,7 @@ const description =
 console.log(description);
  */
 
-/* Lecture: Strings and Template Literals */
+// LECTURE: Strings and Template Literals
 /* 
 const population = 2;
 const language = "Armenian";
@@ -70,7 +70,7 @@ const description = `${country} is in ${continent}, and its ${population} millio
 console.log(description);
  */
 
-// Lecture: Taking Decisions: if / else Statements
+// LECTURE: Taking Decisions: if / else Statements
 /*  
 const population = 2;
 const country = "Armenia";
@@ -82,4 +82,13 @@ if (population > 33) {
     `${country}'s population is ${33 - population} million below average`
   );
 }
+ */
+
+// LECTURE: Type Conversion and Coercion
+/* 
+console.log('9' - '5'); // 4
+console.log("19" - "13" + "17"); // "617"
+console.log("19" - "13" + 17); // 23
+console.log("123" < 57); // false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
  */
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -179,3 +179,60 @@ if (birthYear <= 2000) {
 }
 console.log(century);
  */
+
+// Type Conversion and Coercion
+/* 
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I'm " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" * "2");
+console.log("23" / "2");
+console.log(21 / "3");
+console.log("21" / 3);
+
+let n = "1" + 1; // '11'
+n = n - 1; // 10
+console.log(n);
+ */
+
+// Truthy and Falsy Values
+/* 
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+let money = 0;
+if (money) { // JS will convert money to false because of 0
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+money = 100;
+if (money) { // JS will convert money to false because of 0
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = 0;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
+ */
