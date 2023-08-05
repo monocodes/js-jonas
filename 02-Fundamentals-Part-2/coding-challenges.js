a0e75ace7914e6f60f13473e97c285a4b93d9f0e
monocodes <code@wandering-mono.top>
2023-08-05 23:45:59 +0400

    41-Challenge #2-Video-Solution

--- 02-Fundamentals-Part-2/coding-challenges.js
+++ 02-Fundamentals-Part-2/coding-challenges.js
@@ -8,27 +8,23 @@ Each team competes 3 times, and then the average of the 3 scores is calculated (
 
 A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
 
-
-
 Your tasks:
+1. Create an arrow function calcAverage to calculate the average of 3 scores. This function should have three parameters and return a single number (the average score).
 
-Create an arrow function calcAverage to calculate the average of 3 scores. This function should have three parameters and return a single number (the average score).
+2. Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the calcAverage function to them (you will need to call this function, and pass scores as arguments).
 
-Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the calcAverage function to them (you will need to call this function, and pass scores as arguments).
+3. Create a function checkWinner that takes the average score of each team as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead of hard-coded values).
 
-Create a function checkWinner that takes the average score of each team as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead of hard-coded values).
+4. Use the checkWinner function to determine the winner for both DATA 1 and DATA 2.
 
-Use the checkWinner function to determine the winner for both DATA 1 and DATA 2.
-
-Ignore draws this time. Instead, log No team wins... to the console if there is no winner.
-
-
+5. Ignore draws this time. Instead, log No team wins... to the console if there is no winner.
 
 TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
 
 TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.
  */
-
+/* 
+"use strict";
 const calcAverage = (a, b, c) => (a + b + c) / 3;
 
 // Test Data 1
@@ -50,3 +46,82 @@ scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
 
 checkWinner(scoreDolphins, scoreKoalas);
+ */
+
+//////////////////////////////////////////////////////////////////////////////
+// Coding Challenge #2
+/* 
+Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+Your tasks:
+1. Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+2. And now let's use arrays! So, create an array called bills containing the test data below.
+
+3. Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+4. BONUS: Create an array totals containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555, and 44.
+ */
+/* 
+"use strict";
+
+// Original tip calculator
+// const bill = 275;
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// console.log(
+//   `The bill was ${bill}, the tip was ${tip}, and the total value ${
+//     bill + tip
+//   }.`
+// );
+
+// 1.
+const calcTip = function (bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else return bill * 0.2;
+};
+
+console.log(calcTip(100));
+
+// 2.
+const bills = [125, 555, 44];
+console.log(bills);
+
+// 3.
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+
+// 4.
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(totals);
+ */
+
+// Jonas solution
+// Standard
+/* 
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(bills, tips);
+
+// You can also use an arrow function for calcTip if you prefer that
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+// With Bonus
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
+ */
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -114,6 +114,7 @@ console.log(fruitProcessor(2, 3));
 //////////////////////////////////////////////////////////////////////////////
 // Reviewing Functions
 /* 
+"use strict";
 const calcAge = function (birthYear) {
   return 2037 - birthYear;
 };
@@ -154,3 +155,99 @@ const calcAgeEx = function (birthYear) {
 // Has no 'this' keyword
 const calcAgeAr = (birthYear) => 2037 - birthYear;
  */
+
+//////////////////////////////////////////////////////////////////////////////
+// Introduction to Arrays
+/* 
+"use strict";
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+// friends = ["Bob", "Alice"]; // Uncaught TypeError: Assignment to constant variable.
+
+const firstName = "Jonas";
+const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
+ */
+
+//////////////////////////////////////////////////////////////////////////////
+// Basic Array Operations (Methods)
+
+"use strict";
+
+// Add elements to the array
+// push - add element in the end of the array
+
+const friends = ["Michael", "Steven", "Peter"];
+const newLenth = friends.push("Jay");
+// all functions return something, let's check what will return push function
+console.log(newLenth); // push returns array's lenth
+
+console.log(friends);
+
+// unshift - add element in the beginning of the array
+
+friends.unshift("John");
+console.log(friends);
+
+// Remove elements from an array
+// pop - remove last element from the array
+friends.pop();
+const popped = friends.pop();
+console.log(popped); // pop returns popped element
+console.log(friends);
+
+// shift - remove first element in the array
+friends.shift();
+console.log(friends);
+
+// indexOf - return index of the element
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob")); // -1
+
+// includes - return true or false if the element exists in the array or not
+// works without types coercion
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes("23"));
+console.log(friends.includes(23));
+
+if (friends.includes("Steven")) {
+  console.log("You have a friend called Peter");
+}
