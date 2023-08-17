23f1d1c62cf990c58b04dc5b264cf86f9bc3dc4c
monocodes <code@wandering-mono.top>
2023-08-17 12:38:30 +0400

    06-Modal start   - 80-Working-With-Classes

---
+++ 06-Modal/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {HTMLElement} */
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  // or use CSS property, but do it when you have not many styles
+  // modal.style.display = 'block';
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
