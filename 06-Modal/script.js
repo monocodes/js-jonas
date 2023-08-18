31f115c334ebb1a6826a9913cfc68cd95612bde1
monocodes <code@wandering-mono.top>
2023-08-18 09:24:24 +0400

    06-Modal done   - 07-Pig-Game started   - 82-Project-#3-Pig-Game

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -7,7 +7,7 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('Button clicked');
+  // console.log('Button clicked');
   modal.classList.remove('hidden');
   // or use CSS property, but do it when you have not many styles
   // modal.style.display = 'block';
@@ -25,3 +25,10 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
