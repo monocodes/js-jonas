7904bc9e18cd5d6d6060a5188db3baa8d2858842
monocodes <code@wandering-mono.top>
2023-08-20 01:57:37 +0400

    07-Pig-Game done   - 86-Resetting-the-Game

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -13,15 +13,37 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
+// Starting conditions first
+/* score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
-let playing = true;
+let playing = true; */
+
+let scores, currentScore, activePlayer, playing;
+
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -74,7 +96,7 @@ btnHold.addEventListener('click', function () {
 
     // 2. Check if player's score is >= 100
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -112,3 +134,32 @@ btnHold.addEventListener('click', function () {
     player1El.classList.toggle('player--active');
   }
 }); */
+
+btnNew.addEventListener('click', init);
+// init function is passed to the EventListener as value
+
+// My implementation of Reset button without functions
+/* 
+btnNew.addEventListener('click', function () {
+  // Starting conditions
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  if (activePlayer === 1) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+    document.querySelector(`.player--0`).classList.add('player--active');
+  } else document.querySelector(`.player--0`).classList.add('player--active');
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  scores[0] = 0;
+  scores[1] = 0;
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+});
+ */
