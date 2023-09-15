d49c3001169a7e586810b659fbe51398fe2d886f
monocodes <code@wandering-mono.top>
2023-09-15 13:38:51 +0400

    117-Maps-Fundamentals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -440,7 +440,7 @@ for (const [key, { open, close }] of entries) {
 
 /////////////////////////////////////////////////
 // Sets
-
+/* 
 ('use strict');
 
 const orderSet = new Set([
@@ -479,3 +479,48 @@ console.log(
 console.log(new Set(staff).size);
 
 console.log(new Set('jonasschmedtmann').size);
+ */
+
+/////////////////////////////////////////////////
+// Maps: Fundamentals
+
+// Maps are like objects, but as a key you can have any type of value. In objects all keys are strings.
+
+('use strict');
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+// Chain set method
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(time > rest.get('open') && time < rest.get('close'));
+// console.log(time < rest.get('close'));
+// console.log(time > rest.get('open'));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear(); // delete everything from map
+
+// Arrays as a keys inside maps is possible only as variable
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
